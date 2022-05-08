8947854431ebdf0cd4a16725b1060dda76d40b53
Tausif Fardin <tausiffardin4@gmail.com>
2022-05-08 10:52:55 +0600

    Added the controllers

---
+++ ETickets/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using ETickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
---
+++ ETickets/Controllers/ProducersController.cs
@@ -0,0 +1,29 @@
+﻿using ETickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        //synchronus way to get the data
+        //public IActionResult Index()
+        //{
+        //    var data = _context.Producers.ToList();
+        //    return View();
+        //}
+
+        //asynchronus way to get the data
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
